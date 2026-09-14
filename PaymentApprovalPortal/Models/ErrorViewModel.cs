b434b5413ebe7ae511b2df3cc9059e49e1838454
thepiratt <runhasadilovic@gmail.com>
2026-09-14 14:39:27 +0200

    ran dotnet new mvc

---
+++ PaymentApprovalPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PaymentApprovalPortal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
