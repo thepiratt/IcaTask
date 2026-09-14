2b3104c83e63dd8397e76675ac4cdf5cd9ae56ae
thepiratt <runhasadilovic@gmail.com>
2026-09-14 16:37:39 +0200

    Added views for payments

--- PaymentApprovalPortal/Program.cs
+++ PaymentApprovalPortal/Program.cs
@@ -1,7 +1,10 @@
+using PaymentApprovalPortal.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IPaymentService, PaymentService>();
 
 var app = builder.Build();
 
